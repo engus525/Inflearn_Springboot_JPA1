d26c9aa8b5d8e84e1c5590225fc76c5cb2828c00
kdh <engus525@naver.com>
2023-04-13 19:37:06 +0900

    상품 도메인 개발(entity,repo,service)

--- src/main/java/japbook/jpashop/domain/item/Item.java
+++ src/main/java/japbook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package japbook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import japbook.jpashop.domain.Category;
+import japbook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,23 @@ public abstract class Item //구현체를 만들거라서 추상 class로
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /** 비즈니스 로직 **/
+
+    //stock 증가
+    public void addStock(int quantity)
+    {
+        this.stockQuantity += quantity;
+    }
+
+    //stock 증가
+    public void removeStock(int quantity)
+    {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0)
+        {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
