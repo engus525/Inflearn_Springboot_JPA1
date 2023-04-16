f13b2607f6bfa624d237eef9a959a9f727f4c91a
kdh <engus525@naver.com>
2023-04-16 22:17:11 +0900

    회원 목록 조회

--- src/main/java/japbook/jpashop/controller/MemberForm.java
+++ src/main/java/japbook/jpashop/controller/MemberForm.java
@@ -13,6 +13,11 @@ public class MemberForm
     * domain이 원하는 validation이 다를 수 있다.
     *
     * 서비스가 복잡해지면 코드가 매우 지저분해진다.
+    * !!!Entity는 최대한!! 순수하게 유지하라. 안 그러면 나중에 피눈물을 흘릴 것이다..!!!!!
+    *
+    * + API를 만들 때는 절대 entity를 외부로 반환하지 말자.
+    * 1. private가 노출됨
+    * 2. api 스펙이 변해버림.
     * */
 
     @NotEmpty(message = "회원 이름은 필수입니다.")//build.gradle 수정
